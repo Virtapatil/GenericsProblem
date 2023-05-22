961efbc563da4cc01c9b50e7836b6a59d41b4ad4
Virtapatil2000 <virtapatil01@gmail.com>
2023-05-22 08:41:40 +0530

    Adding 5

---
+++ GenericProblem/GenericMaxMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblem
+{
+    internal class GenericMaxMethod
+    {
+        public static string MaxString(params string[] strings)
+        {
+            if (strings == null || strings.Length == 0)
+            {
+                throw new ArgumentException("The input array must not be empty.");
+            }
+
+            return strings.OrderByDescending(s => s).FirstOrDefault();
+        }
+    }
+}
--- GenericProblem/Program.cs
+++ GenericProblem/Program.cs
@@ -4,12 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int firstNumber = 120;
+            //FindMax
+            /*int firstNumber = 120;
             int secondNumber = 80;
             int thirdNumber = 90;
 
             int maxNumber = FindMax.MaxNumber(firstNumber, secondNumber, thirdNumber);
-            Console.WriteLine($"The maximum number is: {maxNumber}");
+            Console.WriteLine($"The maximum number is: {maxNumber}");*/
+
+            //FindMaxDouble
+            /*double num1 = 5.5;
+            double num2 = 10.2;
+            double num3 = 7.8;
+
+            double maxNumber = FindMaxDouble.MaxNumber(num1, num2, num3);
+            Console.WriteLine($"The maximum number is: {maxNumber}");*/
+
+            //FindMaxString
+            /*string str1 = "apple";
+            string str2 = "banana";
+            string str3 = "orange";
+
+            string maxString = FindMaxString.MaxString(str1, str2, str3);
+            Console.WriteLine($"The maximum string is: {maxString}");*/
+
+            //MaxMethod
+            string maxString = GenericMaxMethod.MaxString("apple", "banana", "orange", "grape");
+            Console.WriteLine($"The maximum string is: {maxString}");
+
+            
         }
     }
 }
